b6ef1c81c1581833fc8905402003ef09ffb56930
Ryan Briones <ryan.briones@brionesandco.com>
2009-08-12 14:51:14 -0400

    add rhtml-mode

--- ini/ruby.el
+++ ini/ruby.el
@@ -1,5 +1,7 @@
 (require 'ruby-mode)
+(require 'rhtml-mode)
 
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rhtml" . rhtml-mode))

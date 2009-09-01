9eb3f91e85db598424cb5f19e582bdcb2e6ce1b6
Ryan Briones <ryan.briones@brionesandco.com>
2009-09-01 15:50:54 -0400

    add rspec and cucumber modes

--- ini/ruby.el
+++ ini/ruby.el
@@ -1,6 +1,9 @@
 (require 'ruby-mode)
 (require 'rhtml-mode)
 
+(require 'rspec-mode)
+(require 'feature-mode)
+
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
